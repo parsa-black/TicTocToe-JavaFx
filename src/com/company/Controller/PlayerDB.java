8c50fe103dcae2a25aa28baec0d55b138f98ba56
Parsa <parsagorgzan1225@gmail.com>
2022-01-13 06:50:44 +0330

    add DataBase in Project

--- src/com/company/Controller/PlayerDB.java
+++ src/com/company/Controller/PlayerDB.java
@@ -1,4 +1,25 @@
 package com.company.Controller;
 
+import java.sql.*;
+
 public class PlayerDB {
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/tiktoktoe-project" ;
+    static final String USER = "root";
+    static final String PASS = "parsagorgzan1225@gmail";
+    private static Connection connection;
+    private static Statement statement;
+
+
+    public static void makeConnection() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            statement = connection.createStatement();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
